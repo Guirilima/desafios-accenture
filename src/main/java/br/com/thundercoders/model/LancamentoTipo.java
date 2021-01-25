d2e1209de3568a0a0e29b43d3c753b53a59caee7
Franklin Barreto <franklin-barreto@hotmail.com>
2021-01-25 13:58:31 -0300

    entidades do plano de contas

---
+++ src/main/java/br/com/thundercoders/model/LancamentoTipo.java
@@ -0,0 +1,6 @@
+package br.com.thundercoders.model;
+
+public enum LancamentoTipo {
+
+	DEBITO,CREDITO,TRANSFERENCIA;
+}
---
+++ src/main/java/br/com/thundercoders/model/PlanoConta.java
@@ -0,0 +1,9 @@
+package br.com.thundercoders.model;
+
+public class PlanoConta {
+
+	private Integer id;
+	private Usuario usuario;
+	private LancamentoTipo lancamentoTipo;
+	private String descricao;
+}
