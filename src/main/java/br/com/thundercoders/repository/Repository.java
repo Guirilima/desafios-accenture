4d00819c9e91fc6ee7ac4fd1932788ff390f64bb
Franklin Barreto <franklin-barreto@hotmail.com>
2021-01-25 12:04:15 -0300

    super interface de repositorio

---
+++ src/main/java/br/com/thundercoders/repository/Repository.java
@@ -0,0 +1,14 @@
+package br.com.thundercoders.repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+
+	public T save(T entity);
+
+	public List<T> findAll();
+
+	public T findById(Integer id);
+
+	public void update(T entity);
+}
