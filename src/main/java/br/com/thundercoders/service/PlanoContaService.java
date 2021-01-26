24848291d02adb7afe04750963cf2b742c8cc943
Franklin Barreto <franklin-barreto@hotmail.com>
2021-01-26 18:17:35 -0300

    plano de conta service e teste

--- src/main/java/br/com/thundercoders/service/PlanoContaService.java
+++ src/main/java/br/com/thundercoders/service/PlanoContaService.java
@@ -10,6 +10,11 @@ public class PlanoContaService {
 	private UsuarioService usuarioService;
 	private PlanoContaRepository planoContaRepository;
 
+	public PlanoContaService(UsuarioService usuarioService, PlanoContaRepository planoContaRepository) {
+		this.usuarioService = usuarioService;
+		this.planoContaRepository = planoContaRepository;
+	}
+
 	public PlanoConta salvaPlanoConta(DtoPlanoConta dtoPlanoConta) {
 		Usuario usuario = usuarioService.findById(dtoPlanoConta.getUsuarioId());
 		return planoContaRepository.save(new PlanoConta(usuario, dtoPlanoConta.getDescricao()));
