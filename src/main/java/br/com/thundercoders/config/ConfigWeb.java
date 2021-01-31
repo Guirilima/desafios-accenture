928664b4b5dcaf3a7b3717ddfbc99010e03585b1
Caio A. Capasso <caiocapasso@yahoo.com.br>
2021-01-31 20:12:40 -0300

    ajustes para implementar swagger

--- src/main/java/br/com/thundercoders/config/ConfigWeb.java
+++ src/main/java/br/com/thundercoders/config/ConfigWeb.java
@@ -1,5 +1,6 @@
 package br.com.thundercoders.config;
 
+import br.com.thundercoders.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,38 +14,34 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import br.com.thundercoders.service.UsuarioService;
-
 @EnableWebSecurity
 @Configuration
 public class ConfigWeb extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private UsuarioService jpaUserDetailService;
-	@Autowired
-	private TokenService tokenService;
+    private static final String[] SWAGGER_WHITELIST = {"/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui.html", "/webjars/**"};
+    @Autowired
+    private UsuarioService jpaUserDetailService;
+    @Autowired
+    private TokenService tokenService;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/usuario/logar", "/usuario/*").permitAll().anyRequest().authenticated()
-				.and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-				.addFilterBefore(new JWTAuthorizationFilter(tokenService, jpaUserDetailService),
-						UsernamePasswordAuthenticationFilter.class);
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/usuario/logar", "/usuario/*").permitAll().antMatchers(SWAGGER_WHITELIST).permitAll().anyRequest().authenticated().and().cors().and().csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().addFilterBefore(new JWTAuthorizationFilter(tokenService, jpaUserDetailService), UsernamePasswordAuthenticationFilter.class);
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(jpaUserDetailService).passwordEncoder(passwordEncoder());
-	}
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(jpaUserDetailService).passwordEncoder(passwordEncoder());
+    }
 
-	@Override
-	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
